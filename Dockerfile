003a188937deb415c09835849102ef151e390cf0
edgefox <ivanlyutov@gmail.com>
2014-06-24 20:11:28 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM stealthly/docker-java
+
+MAINTAINER stealthly
+
+ENV PLAY_VERSION 2.2.3
+ENV PLAY_RELEASE play-$PLAY_VERSION
+ENV PLAY_URL http://downloads.typesafe.com/play/$PLAY_VERSION/$PLAY_RELEASE.zip
+ENV PLAY_HOME /opt/$PLAY_RELEASE
+
+ENV KAFKA_CONSOLE_URL https://github.com/claudemamo/kafka-web-console.git
+ENV KAFKA_CONSOLE_HOME /opt/kafka-web-console
+
+RUN wget -q $PLAY_URL -O /tmp/$PLAY_RELEASE.zip
+RUN unzip /tmp/$PLAY_RELEASE.zip -d /opt
+
+RUN mkdir -p $KAFKA_CONSOLE_HOME && cd $KAFKA_CONSOLE_HOME && git clone $KAFKA_CONSOLE_URL
+
+ADD play /usr/bin/play
+
+EXPOSE 8080
+
+CMD cd $KAFKA_CONSOLE_HOME && play
---
+++ start-console.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#!/bin/bash
+ZOOKEEPER=`docker ps -a | awk '{print $NF}'  | grep "zookeeper$"`
+ZOOKEEPER_RUNNING=$?
+if [ $ZOOKEEPER_RUNNING -eq 0 ] ;
+then
+    echo "Zookeeper is already running"
+else
+    echo "Starting Zookeeper"
+    docker run -p 2181:2181  -h zkserver --name zkserver -d stealthly/docker-zookeeper
+fi
+
+ID=$1
+PORT=$2
+HOST_IP=$3
+
+docker run --name=broker$ID -p $PORT:$PORT --link zkserver:zk -e BROKER_ID=$ID -e HOST_IP=$HOST_IP -e PORT=$PORT -d stealthly/docker-kafka
+docker run --name=kafka-console -p 8080:8080 --link zkserver:localhost --link broker$ID:localhost -e PORT=$PORT -d stealthly/docker-kafka-webconsole
